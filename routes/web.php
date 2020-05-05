bfc95f4584516f26cf7f5726909ae3be55f91eca
Jpfmartinez <juanpablofloresmartinez@gmail.com>
2020-05-05 16:07:16 -0500

    commit para vistas

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home')->name('home');
+Route::view('/mostrar', 'mostrar')->name('mostrar');
+Route::view('/agregar', 'agregar')->name('agregar');

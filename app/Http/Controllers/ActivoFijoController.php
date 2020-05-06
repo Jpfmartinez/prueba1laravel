5bfbae75419683e8b1956e07365b93991c962a08
graciela24 <mariagraciela_torres@ucol.mx>
2020-05-05 19:05:12 -0500

    creacion de modelo 2

--- app/Http/Controllers/ActivoFijoController.php
+++ app/Http/Controllers/ActivoFijoController.php
@@ -14,6 +14,7 @@ class ActivoFijoController extends Controller
     public function index()
     {
         //
+        
     }
 
     /**
--- app/activofijomodel.php
+++ app/activofijomodel.php
@@ -4,8 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class activofijomodel extends Model
+class ActivoFijoModel extends Model
 {
-    protected $table='activofijo';
-    protected $fillable = ['numInventario', 'tipo', 'descripcion','numSerie','resguardo','valorAdqui','verificado','numTrabajador'];
+  protected $table='activofijo';
+  protected $fillable = ['numInventario', 'tipo', 'descripcion','numSerie','resguardo','valorAdqui','verificado','numTrabajador'];
 }

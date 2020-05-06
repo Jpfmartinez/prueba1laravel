7c6cb5579170cd5f9a34630020755fc09207b161
Jpfmartinez <juanpablofloresmartinez@gmail.com>
2020-05-05 19:27:03 -0500

    VISTA PARA MOSTRAR

--- resources/views/mostrar.blade.php
+++ resources/views/mostrar.blade.php
@@ -1,5 +1,36 @@
 @extends('layout')
 
 @section('content')
-	<h1>MOSTAR</h1>
+<h1>MOSTAR</h1>
+<table>
+	<thead>
+		<tr>
+			<th>ID</th>
+			<th>No de Inventario</th>
+			<th>Tipo</th>
+			<th>Descripcion</th>
+			<th>No de Serie</th>
+			<th>Resguardo</th>
+			<th>Valor de adquisicion</th>
+			<th>Verificado</th>
+			<th>No de Trabajador</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($activos as $activo)
+		<tr>
+			<td>{!! $activo->id !!}</td>
+			<td>{!! $activo->numInventario !!}</td>
+			<td>{!! $activo->tipo !!}</td>
+			<td>{!! $activo->descripcion !!}</td>
+			<td>{!! $activo->numSerie !!}</td>
+			<td>{!! $activo->resguardo !!}</td>
+			<td>{!! $activo->valorAdqui !!}</td>
+			<td>{!! $activo->verificado !!}</td>
+			<td>{!! $activo->numTrabajador !!}</td>
+
+		</tr>
+		@endforeach
+	</tbody>
+</table>
 @endsection
